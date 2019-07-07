c747cfcb06b91dcb36ca033ff527f71703cc456d
stravis11 <stevetravis11@gmail.com>
2019-07-07 14:41:33 -0400

    Footer styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./Header.css";
+import Hero from "../Hero";
 
 const Header = props => (
   <div className="header">
+    {/* <Hero backgroundImage="https://i.imgur.com/2ST6V90.png"> */}
     <h1>{props.title}</h1>
     <h2>{props.instructions}</h2>
+    {/* </Hero> */}
   </div>
 );
 export default Header;
