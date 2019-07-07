b65358e0f4d7d610cedf3297a065952872fa4b3b
stravis11 <stevetravis11@gmail.com>
2019-07-07 13:18:04 -0400

    Initial static page rendering. No styling yet.

--- src/App.js
+++ src/App.js
@@ -4,38 +4,53 @@ import Header from "./components/Header";
 import ImageCard from "./components/ImageCard";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import cities from "./characters.json";
+import characters from "./characters.json";
 
-// Render the page
-render();
-return (
-  <div className="App">
-    <Navbar
-      title={"Clicky Game"}
-      msg={this.state.gameMsg}
-      score={this.state.currentScore}
-      topScore={this.state.topScore}
-    />
-    <Header
-      title={"Stranger Things Character"}
-      instructions={
-        "Test your memory. See how many times you can select a character only once!"
-      }
-    />
+// Set max score
+const maxScore = characters.length;
 
-    <Wrapper>
-      {characters.map(characters => (
-        <ImageCard
-          key={character.id}
-          id={character.id}
-          name={character.name}
-          image={character.image}
-          selectImage={this.selectImage}
+class App extends Component {
+  // Set this.state.characters to the characters json array
+  state = {
+    characters,
+    currentScore: 0,
+    topScore: 0,
+    selectedImages: [],
+    gameMsg: "Click a character to begin!"
+  };
+
+  // Render the page
+  render() {
+    return (
+      <div className="App">
+        <Navbar
+          title={"Clicky Game"}
+          msg={this.state.gameMsg}
+          score={this.state.currentScore}
+          topScore={this.state.topScore}
         />
-      ))}
-    </Wrapper>
-    <Footer title={"Stranger Things Clicky Game"} />
-  </div>
-);
+        <Header
+          title={"Stranger Things Characters"}
+          instructions={
+            "Test your memory. See how many times you can select a character only once!"
+          }
+        />
+
+        <Wrapper>
+          {characters.map(character => (
+            <ImageCard
+              key={character.id}
+              id={character.id}
+              name={character.name}
+              image={character.image}
+              selectImage={this.selectImage}
+            />
+          ))}
+        </Wrapper>
+        <Footer title={"Stranger Things Clicky Game"} />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-mport React from "react";
+import React from "react";
 import "./Header.css";
 
 const Header = props => (
